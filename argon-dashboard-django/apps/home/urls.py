30833fbf83286ca89299b8ab6fcce22203fa22af
An Tran <antran@Ans-MacBook-Pro.local>
2023-03-31 19:49:40 -0400

    Add upload .pkl file and csv file in the error analysis feature

--- argon-dashboard-django/apps/home/urls.py
+++ argon-dashboard-django/apps/home/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('errorAnalysis/', views.errorAnalysis, name='errorAnalysis'),
     path('useCases/', views.useCases, name='useCases'),
     path('cleaning/upload',views.upload,name='upload'),
+<<<<<<< HEAD
     path('create/upload',views.upload,name='upload'),
     path('retrain/upload',views.upload,name='upload'),
     path('cleaning/getDataset',views.getDataset,name='getDataset'),
@@ -27,6 +28,11 @@ urlpatterns = [
     path('merge/getDataset',views.getDataset,name='getDataset'),
     path('merge/upload',views.upload,name='upload'),
     path('merge/getDatasetCols',views.getDatasetCols,name='getDatasetCols'),
+=======
+    path('errorAnalysis/upload', views.upload, name='upload'),
+    #path('cleaning/impute',views.impute,name='impute'),
+    #path('cleaning/dropFeature',views.dropFeature,name='dropFeature'),
+>>>>>>> 546a974 (Add upload .pkl file and csv file in the error analysis feature)
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
