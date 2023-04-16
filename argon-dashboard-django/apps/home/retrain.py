777f3ef98e109d2fa739ff206d55005bf5462861
Johann Christoph Winter <cheesemid@protonmail.com>
2023-04-16 19:38:00 -0400

    update model retraining and creation
    more model types
    retraining support for models without partial_fit
    multiple epochs

--- argon-dashboard-django/apps/home/retrain.py
+++ argon-dashboard-django/apps/home/retrain.py
@@ -11,54 +11,98 @@ import pandas as pd
 import numpy as np
 import pickle
 
-from sklearn.linear_model import SGDRegressor, SGDClassifier
+from sklearn.linear_model import SGDRegressor, SGDClassifier, LogisticRegression, LinearRegression
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 
 def retrain_model(request):
 
-    with open('./users/'+request.user.username+'/model.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
+    with open('./users/'+request.user.username+'/model_dict.pkl', 'rb') as model_file:
+        model_dict = pickle.load(model_file)
 
-    data = pd.read_csv('./users/'+request.user.username+'/'+request.POST['fileName'],skipinitialspace=True)
+    new_data = pd.read_csv('./users/'+request.user.username+'/'+request.POST['fileName'],skipinitialspace=True)
 
-    prediction_column = request.POST["feature1"]
+    if list(new_data.columns) != model_dict["features"]:
+        return HttpResponse("")
 
-    y = data[prediction_column]
+    model = model_dict["model"]
+    # prediction_column = request.POST["feature1"]
+    prediction_column = model_dict["prediction_column"]
 
-    data.drop(prediction_column, axis=1, inplace=True)
+    if model_dict["retrainable"] == True:
+
+        y = new_data[prediction_column]
+
+        new_data.drop(prediction_column, axis=1, inplace=True)
+        
+        x = new_data
+
+        model.fit(x,y)
+
+    elif model_dict["retrainable"] == False:
+
+        ds_list = []
+
+        for ds in model_dict["model_ds"]:
+            ds_list.append(pd.read_csv('./users/'+request.user.username+'/'+ds,skipinitialspace=True))
+        
+        ds_list.append(new_data)
+
+        data = pd.concat(ds_list)
+
+        y = data[prediction_column]
+
+        data.drop(prediction_column, axis=1, inplace=True)
+
+        x = data
+
+        model.fit(x,y)
     
-    x = data
-    
-    
-    
-    model.partial_fit(x,y)
+    model_dict["model"] = model
+    model_dict["model_ds"].append(request.POST['fileName'])
 
-    model_pkl = pickle.dumps(model)
+    model_pkl = pickle.dumps(model_dict)
 
-    with open('./users/'+request.user.username+'/model.pkl', 'wb') as model_file:
+    with open('./users/'+request.user.username+'/model_dict.pkl', 'wb') as model_file:
         model_file.write(model_pkl)
 
     response = HttpResponse(
                     model_pkl,
                     headers={
                         "Content-Type": "application/octet-stream",
-                        "Content-Disposition": 'attachment; filename="model.pkl"'
+                        "Content-Disposition": 'attachment; filename="model_dict.pkl"'
                     }
                 )
     
     return response
 
 def create_model(request):
-    # print(list(request.POST.items()))
-
-    
 
     data = pd.read_csv('./users/'+request.user.username+'/'+request.POST['fileName'],skipinitialspace=True)
+    retrainable = False
+
+    features = list(data.columns)
 
     if request.POST["model_type"] == "SGDRegressor":
         model = SGDRegressor()
+        retrainable = True
     elif request.POST["model_type"] == "SGDClassifier":
         model = SGDClassifier()
+        retrainable = True
+    elif request.POST["model_type"] == "LogisticRegression":
+        model = LogisticRegression()
+        retrainable = False
+    elif request.POST["model_type"] == "LinearRegression":
+        model = LinearRegression()
+        retrainable = False
+    elif request.POST["model_type"] == "DecisionTreeClassifier":
+        model = DecisionTreeClassifier()
+        retrainable = False
+    elif request.POST["model_type"] == "DecisionTreeRegressor":
+        model = DecisionTreeRegressor()
+        retrainable = False
+    else:
+        return HttpResponse("")
 
     prediction_column = request.POST["feature1"]
 
@@ -71,17 +115,26 @@ def create_model(request):
     
     model.fit(x,y)
 
-    model_pkl = pickle.dumps(model)
+    model_dict = {
+                    "model_type": request.POST["model_type"],
+                    "prediction_column": prediction_column,
+                    "features": features,
+                    "model": model,
+                    "retrainable": retrainable,
+                    "model_ds": [request.POST['fileName']]
+                }
+
+    model_pkl = pickle.dumps(model_dict)
 
     response = HttpResponse(
                     model_pkl,
                     headers={
                         "Content-Type": "application/octet-stream",
-                        "Content-Disposition": 'attachment; filename="model.pkl"'
+                        "Content-Disposition": 'attachment; filename="model_dict.pkl"'
                     }
                 )
 
-    with open('./users/'+request.user.username+'/model.pkl', 'wb') as model_file:
+    with open('./users/'+request.user.username+'/model_dict.pkl', 'wb') as model_file:
         model_file.write(model_pkl)
     
     return response
