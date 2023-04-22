841de692cdd0b043fbb649b3404303ef6cefb0f4
An Hai Tran <anhaitran.cs@gmail.com>
2023-04-21 22:18:25 -0400

    Fixing stuff

--- argon-dashboard-django/apps/home/erroranalysis.py
+++ argon-dashboard-django/apps/home/erroranalysis.py
@@ -16,16 +16,14 @@ import socket
 def createDashboard(request):
     df=pd.read_csv('./users/'+request.user.username+'/'+request.POST['fileName'],skipinitialspace=True) #Turns all blank cells into NaN
     model = pickle.load(open('./users/'+request.user.username+'/'+request.POST['modelName'], 'rb'))
-    training_features = model.feature_names_in_
-    column_names = set(df.columns)
-    testing_feature = list(column_names-set(training_features))
+    testing_feature = ['admit']
     y_test = df[testing_feature]
     X_test = df.drop(testing_feature, axis=1)
     y_pred = model.predict(X_test)
+    training_features = list(X_test.columns)
     #find a free port
     sock = socket.socket()
     sock.bind(('', 0))
     port = sock.getsockname()[1]
-    print(type(X_test), type(y_test), type(training_features), type(y_pred))
     ErrorAnalysisDashboard(dataset=X_test, true_y=np.ravel(y_test), port=port, features=training_features, pred_y=y_pred)
     return port
